7e10a0291a260dc7cf09f7b59633de68eeef8be2
dpark26 <danielpark0416@gmail.com>
2024-08-08 13:33:41 -0400

    finishing up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from '@astrojs/react';
+import icon from "astro-icon";
 export default defineConfig({
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), ]
+  integrations: [tailwind(), sitemap(), react(), icon()]
 });
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,53 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import styles from './Carousel.module.css';
+
+export default function Carousel() {
+    return (
+        <Swiper
+            modules={[Navigation]}
+            navigation
+            spaceBetween={10}
+            slidesPerView={1}
+            className={styles['swiper-container']}
+        >
+            <SwiperSlide className={styles['carousel-container']}>
+                <img
+                    className={styles['carousel-img']}
+                    src="/images/grad.jpg"
+                    alt="Graduation"
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles['carousel-container']}>
+                <img
+                    className={styles['carousel-img']}
+                    src="/images/research_team.JPG"
+                    alt="Research Awards"
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles['carousel-container']}>
+                <img
+                    className={styles['carousel-img']}
+                    src="/images/eng_team.jpg"
+                    alt="Engineering Team"
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles['carousel-container']}>
+                <img
+                    className={styles['carousel-img']}
+                    src="/images/cfg.JPG"
+                    alt="Camp Flog Gnaw"
+                />
+            </SwiperSlide>
+            <SwiperSlide className={styles['carousel-container']}>
+                <img
+                    className={styles['carousel-img']}
+                    src="/images/zoom.jpg"
+                    alt="Zoom"
+                />
+            </SwiperSlide>
+        </Swiper>
+    );
+}
