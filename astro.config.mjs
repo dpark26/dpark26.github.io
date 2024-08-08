d96d92f1eb75f3d10e573104e08db22d5ca2df6d
dpark26 <danielpark0416@gmail.com>
2024-08-08 13:38:30 -0400

    updated readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
 import icon from "astro-icon";
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
+  site: 'https://dpark26.github.io',
   integrations: [tailwind(), sitemap(), react(), icon()]
 });
